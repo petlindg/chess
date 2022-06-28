75af9c45a13154f73caf194daf1692fd01ba29f1
Petter Lindgren <petlindg@student.chalmers.se>
2022-06-29 01:00:23 +0200

    w/e

--- Chess.java
+++ Chess.java
@@ -4,41 +4,61 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class Chess extends Application{
+public class Chess extends Application {
 
+    final private int squareSize = 100;
+    final private int squareNum = 8;    
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
-    
-    Rectangle r1 = new Rectangle();
-    r1.setX(0);
-    r1.setY(0);
-    r1.setWidth(100);
-    r1.setHeight(100);
-    r1.setFill(Color.rgb(100,100,100));
-    
-    Rectangle r2 = new Rectangle();
-    r2.setX(100);
-    r2.setY(100);
-    r2.setWidth(100);
-    r2.setHeight(100);
-    r2.setFill(Color.rgb(255,255,255));
-    
-    
-    StackPane root = new StackPane();
-    root.getChildren().addAll(r1,r2);
-    Scene scene = new Scene(root);
-    
-    primaryStage.setScene(scene);
-    primaryStage.setTitle("Schack");
-    primaryStage.show();
+        
+        Pane root = new Pane();
+        Scene scene = new Scene(root);
+        
+        plotGrid(root, squareSize, squareNum);
+        
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Chess");
+        primaryStage.show();
+        
     }
     
     public static void main(String[] args) {
         launch(args);
     }
     
-    //public void drawBoard()
-    
+    public void plotGrid(Pane root, int squareSize, int squareNum) {
+        
+        Color c1 = Color.rgb(243,240,235); //white
+        Color c2 = Color.rgb(116,179,119); //green
+        Color c;
+        Rectangle[] squares = new Rectangle[squareNum*squareNum];
+        int n = 0;
+        
+        for(int i = 0; i<squareNum; i++) {
+        
+            for(int j = 0; j<squareNum; j++) {
+            
+                if((i+j)%2==0) {
+                    c = c1;
+                }
+                else {
+                    c = c2;
+                }
+                squares[n] = new Rectangle(j*squareSize, i*squareSize, squareSize, squareSize);
+                squares[n].setFill(c);
+                n++;
+                
+            }
+            
+        }
+        
+        root.getChildren().addAll(squares);
+        
+    }
+       
 }
